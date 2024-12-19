0247eda8575ff79aa1e2aea523bea37bf122497b
hieule2309 <qhieule2309@gmail.com>
2024-12-19 11:42:34 +0700

    Update: Cập nhật các provider đăng kí các service interface

---
+++ app/Providers/CategoryServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Interfaces\CategoryRepositoryInterface;
+use App\Repositories\CategoryRepository;
+
+class CategoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
